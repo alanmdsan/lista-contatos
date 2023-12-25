2ce582b520fa2b91da27f3c629d9b9dd1bb0e073
alanmdsan <alanmdsan@gmail.com>
2023-12-25 15:46:13 -0300

    setting remove action

--- src/components/Contato/index.tsx
+++ src/components/Contato/index.tsx
@@ -1,27 +1,86 @@
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
 import { Icon } from '@iconify/react'
-import * as S from './styles'
 
-const Contato = () => {
+import * as S from './styles'
+import ContatoModel from '../../models/Contato'
+import { remover } from '../../store/reducers/contatos'
+
+type Props = ContatoModel
+
+const Contato = ({
+  id,
+  nome: nomeOriginal,
+  email: emailOriginal,
+  telefone: telefoneOriginal
+}: Props) => {
+  const dispatch = useDispatch()
+  const [estaEditando, setEstaEditando] = useState(false)
+  const [nome, setNome] = useState('')
+  const [email, setEmail] = useState('')
+  const [telefone, setTelefone] = useState('')
+
+  useEffect(() => {
+    setNome(nomeOriginal)
+    setEmail(emailOriginal)
+    setTelefone(telefoneOriginal)
+  }, [nomeOriginal, emailOriginal, telefoneOriginal])
+
+  function cancelaEdicao() {
+    setEstaEditando(false)
+    setNome(nomeOriginal)
+    setEmail(emailOriginal)
+    setTelefone(telefoneOriginal)
+  }
+
   return (
     <S.Card>
       <S.CardHeader>
-        <h1>Alan Santos</h1>
+        <h1>{nome}</h1>
         <div>
-          <Icon
-            style={{ cursor: 'pointer' }}
-            icon="material-symbols:edit-outline"
-            width="24"
-            height="24"
-          />
+          {estaEditando ? (
+            <>
+              <Icon
+                style={{ cursor: 'pointer' }}
+                icon="line-md:confirm-circle"
+                width="24"
+                height="24"
+              />
+              <Icon
+                style={{ cursor: 'pointer', marginLeft: '8px' }}
+                icon="material-symbols:cancel-outline"
+                width="24"
+                height="24"
+                onClick={cancelaEdicao}
+              />
+            </>
+          ) : (
+            <>
+              <Icon
+                style={{ cursor: 'pointer' }}
+                icon="material-symbols:edit-outline"
+                width="24"
+                height="24"
+                onClick={() => setEstaEditando(true)}
+              />
+              <Icon
+                style={{ cursor: 'pointer', marginLeft: '8px' }}
+                icon="mdi:trash-outline"
+                width="24"
+                height="24"
+                onClick={() => dispatch(remover(id))}
+              />
+            </>
+          )}
         </div>
       </S.CardHeader>
       <S.PhoneContainer>
         <Icon icon="ph:phone" width="24" height="24" />
-        <h2>(32) 98594-3433</h2>
+        <h2>{telefone}</h2>
       </S.PhoneContainer>
       <S.MailContainer>
         <Icon icon="material-symbols:mail-outline" width="24" height="24" />
-        <h2>alan@gmail.com</h2>
+        <h2>{email}</h2>
       </S.MailContainer>
     </S.Card>
   )
--- src/containers/ListaDeContatos/index.tsx
+++ src/containers/ListaDeContatos/index.tsx
@@ -1,11 +1,23 @@
+import { useSelector } from 'react-redux'
+import { RootReducer } from '../../store'
 import Contato from '../../components/Contato'
 
-const ListaDeContatos = () => (
-  <>
-    <Contato />
-    <Contato />
-    <Contato />
-  </>
-)
+const ListaDeContatos = () => {
+  const { itens } = useSelector((state: RootReducer) => state.contatos)
+
+  return (
+    <>
+      {itens.map((i) => (
+        <Contato
+          key={i.id}
+          id={i.id}
+          nome={i.nome}
+          email={i.email}
+          telefone={i.telefone}
+        />
+      ))}
+    </>
+  )
+}
 
 export default ListaDeContatos
